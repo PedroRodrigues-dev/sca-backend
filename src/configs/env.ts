ac74a2f4b87023bdce2596c10053e55b3e64e863
Pedro Rodrigues <pedroras004@gmail.com>
2023-10-25 19:50:52 -0300

    Implementa postgres

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -1,1 +1,6 @@
-export const JWT_SECRET_KEY = "mysupersecret";
+export const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || "mysupersecret";
+export const DATABASE_HOST = process.env.DATABASE_HOST || "localhost";
+export const DATABASE_PORT = parseInt(process.env.DATABASE_PORT || "5432", 10);
+export const DATABASE_NAME = process.env.DATABASE_NAME || "sca";
+export const DATABASE_USER = process.env.DATABASE_USER || "postgres";
+export const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD || "postgres";
